02c397f139de0f9f4a80b6b0ffa3efdbe516b705
Remy Navarre <rnavarre@usc.edu>
2025-09-13 00:28:14 +0000

    done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,32 @@
 
 int main(int argc, char* argv[])
 {
+  ULListStr testList;
+  std::cout<< "Empty list has size: " << testList.size()<< std::endl;
+  testList.push_back("a");
+  testList.push_back("b");
+  testList.push_back("c");
+  std::cout << "pushed " <<testList.size() << "elements"<<std::endl;
+  std::cout <<"front: " << testList.front() << ", back: " << testList.back() << std::endl;
 
+  testList.push_front("x");
+  testList.push_front("y");
+  testList.push_front("z");
+  std::cout << "size after pushing three more elements:  " <<testList.size() << "elements"<<std::endl;
+  std::cout <<"front: " << testList.front() << ", back: " << testList.back() << std::endl;
+
+  //std::cout<<"element at 4th index: " << testList.getValAtLoc(4) << std::endl;
+
+  testList.pop_back();
+  std::cout << "size after popping back: " << testList.size() << ", and new last element is:  " << testList.back() << std::endl;
+  testList.pop_front();
+  std::cout << "size after popping front: " << testList.size() << ", and new first element is:  " << testList.front() << std::endl;
+
+  std::cout << "the final list is: ";
+  for(size_t i = 0; i < testList.size(); i++){
+    std::cout << testList.get(i) << ", ";
+  }
+  std::cout << std::endl;
+
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,180 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+/**
+   * Adds a new value to the back of the list
+   *   - MUST RUN in O(1)
+   */
+void ULListStr::push_back(const std::string& val)
+{
+  if(head_ == nullptr){
+    // list is empty
+    Item* firstItem = new Item();
+    head_ = firstItem; 
+    tail_ = firstItem; 
+    head_->val[head_->first] = val;
+    firstItem->last++;
+    size_++;
+  }
+  else{
+    if(tail_->last+1 <= ARRSIZE ){ 
+      // theres room so add the val
+      tail_->val[tail_->last] = val;
+      tail_->last = tail_->last+1;
+      size_++;
+    }
+    else{
+      // no room so need to alloc a new item
+      Item* lastItem = new Item();
+      tail_->next = lastItem;
+      lastItem->prev = tail_; 
+      tail_= lastItem;
+      size_++;
+      tail_->val[tail_->last] = val;
+      tail_->last ++;
+    }
+  }
+}
+
+/**
+  * Removes a value from the back of the list
+  *   - MUST RUN in O(1)
+  */
+void ULListStr::pop_back()
+{
+  if(head_ == nullptr){
+    return;
+  }
+
+  tail_->last --;
+  size_--;
+
+  // if popping the last remaining elelemnt in an item, remove the item, deallocate
+  if(tail_->last - tail_->first == 0){
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    if(tail_!=nullptr){
+      tail_->next = nullptr;
+    }
+    else{
+      head_ = nullptr;
+    }
+    delete temp;
+  }
+}
+
+/**
+  * Adds a new value to the front of the list.
+  * If there is room before the 'first' value in
+  * the head node add it there, otherwise, 
+  * allocate a new head node.
+  *   - MUST RUN in O(1)
+  */
+void ULListStr::push_front(const std::string& val)
+{
+  if(head_ == nullptr){
+    // list is empty
+    Item* firstItem = new Item();
+    head_ = firstItem; 
+    tail_ = firstItem; 
+    // adding val in the first spot of the array
+    head_->val[head_->first] = val;
+    firstItem->last++;
+    size_++;
+    return;
+  }
+  if(head_->first > 0){
+    // theres room so add the val
+    head_->first--;
+    head_->val[head_->first] = val; 
+    size_++;
+  }
+  else{
+    // no room so need to alloc a new item
+    Item* firstItem = new Item();
+    firstItem ->first = firstItem->last = ARRSIZE;
+    head_->prev = firstItem;
+    firstItem->next = head_; 
+    head_= firstItem;
+    
+    head_->first --;
+    head_->val[head_->first] = val;
+    size_++;
+  }
+  
+}
+
+/**
+  * Removes a value from the front of the list
+  *   - MUST RUN in O(1)
+  */
+void ULListStr::pop_front()
+{
+  if(head_ == nullptr){
+    return;
+  }
+
+  head_->first++;
+  size_--;
+
+  // if popping the last remaining elelemnt in an item, remove the item, deallocate
+  if(head_->last == head_->first){
+    Item* temp = head_;
+    head_ = head_->next;
+    if(head_!=nullptr){
+      head_->prev = nullptr;
+    }
+    else{
+      tail_ = nullptr;
+    }
+    delete temp;
+  }
+}
+
+/**
+  * Returns a const reference to the back element
+  *   - MUST RUN in O(1)
+  */
+std::string const & ULListStr::back() const
+{
+  return tail_->val[tail_->last-1];
+}
+
+/**
+  * Returns a const reference to the front element
+  *   - MUST RUN in O(1)
+  */
+std::string const & ULListStr::front() const
+{
+  return head_->val[head_->first];
+}
+
+/** 
+   * Returns a pointer to the item at index, loc,
+   *  if loc is valid and NULL otherwise
+   *   - MUST RUN in O(n) 
+   */
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  if(loc >= size_){
+    return NULL;
+  }
+
+  Item* temp = head_;
+  size_t totalIndex = 0;
+
+  while(temp != nullptr){
+    size_t numElements = temp->last - temp->first;
+    if(loc < numElements + totalIndex){
+      size_t indexInItem = loc - totalIndex;
+      return &(temp->val[temp->first + indexInItem]);
+    }
+    totalIndex += numElements;
+    temp = temp->next;
+  }
+  return NULL;
+
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
